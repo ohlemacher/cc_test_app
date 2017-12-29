7fba1b5c99dd582e9431752c5500557948b476cf
David Ohlemacher <ohlemacher@us.ibm.com>
2017-12-29 16:18:44 -0500

    Running app

---
+++ test_app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from cc_test_module.cc_test_module import rot13_encode
+import argparse
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--in",
+                        help="Input string to rot13 encode",
+                        action="store",
+                        dest='ins',
+                        required=True)
+    results = parser.parse_args()
+
+
+    inputs = dict()
+    inputs['ins'] = results.ins
+    return inputs
+
+
+if __name__ == '__main__':
+    args = arg_parse()
+
+    print(rot13_encode(args['ins']))
+
