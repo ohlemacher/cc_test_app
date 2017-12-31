b7ff1e3f04c8af26ad8ba465a48c142782943b5d
David Ohlemacher <ohlemacher@us.ibm.com>
2017-12-31 13:26:35 -0500

    Fix paths

--- test_app.sh
+++ test_app.sh
@@ -8,8 +8,8 @@ if [ "$(hostname)" == "pluto.local" ]; then
     declare -r cc_test_app_dir="cc_test_app"
     declare -r cc_test_module_dir="cc_test_module"
 else
-    declare -r cc_test_app_dir="cc-test-app-repo"
-    declare -r cc_test_module_dir="cc-test-module-repo"
+    declare -r cc_test_app_dir="cc_test_app"
+    declare -r cc_test_module_dir="cc_test_module"
 fi
 
 function info {
@@ -27,16 +27,18 @@ function prereqs {
     pip install pytest || die "pip install pytest failed"
 
     if [ "$(hostname)" != "pluto.local" ]; then
-        echo "find $cc_test_module_dir..."
+        info "find $cc_test_module_dir..."
         find / -name "$cc_test_module_dir"
     fi
 
+    info "install cc_test_module"
     pushd "$cc_test_module_dir" > /dev/null
     pip install -e . || die "pip -e cc-test-module-repo install failed"
     popd > /dev/null
 }
 
 function run_unit_tests {
+    info "Run unit tests for app.py"
     pushd "$cc_test_app_dir" > /dev/null
     python -m pytest -v test_app.py || die "app.py unit tests failed"
     popd > /dev/null
