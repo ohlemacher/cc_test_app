917f9b217b5d63ff184cdddbbedf13d83ba3bb1e
David Ohlemacher <ohlemacher@us.ibm.com>
2018-01-01 15:47:08 -0500

    git

--- commit_to_passed_branch.sh
+++ commit_to_passed_branch.sh
@@ -29,15 +29,14 @@ function install_prereqs {
 
 function commit_to_branch {
     local -r br="$1"
-    info "Commit to $br branch"
     pushd "$cc_test_app_dir" > /dev/null
-    echo "pwd: $(pwd)"
-    echo "branch: $(git branch)"
+    local -r head="$(git rev-parse HEAD)"
+    info "Commit to $br branch. Move HEAD to $head"
     #git fetch origin develop           || die "git fetch develop failed"
-    #git fetch origin "$br"             || die "git fetch $br failed"
-    #git checkout -b "$br" "origin/$br" || die "git checkout $br failed"
-    git checkout "$br"                 || die "git checkout $br failed"
-    git rebase develop                 || die "git rebase develop for $br failed"
+    git fetch origin "$br"             || die "git fetch $br failed"
+    git checkout -b "$br" "origin/$br" || die "git checkout $br failed"
+    #git checkout "$br"                 || die "git checkout $br failed"
+    git rebase "$head"                 || die "git rebase develop for $br failed"
     git push "$br"                     || die "git push origin $br failed"
     popd > /dev/null
 }
