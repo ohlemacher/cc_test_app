6c4d45f117d1ffecec8be45283f8730daea2f872
David Ohlemacher <ohlemacher@us.ibm.com>
2018-01-01 20:52:38 -0500

    git

--- commit_to_passed_branch.sh
+++ commit_to_passed_branch.sh
@@ -33,6 +33,9 @@ function commit_to_branch {
     local -r head="$(git rev-parse HEAD)"
     info "Commit to $br branch. Move HEAD to $head"
 
+    info "+++ cat .git/config"
+    cat .git/config
+
     #info "+++ git remote update"
     #git remote update                  || die "git remote update failed"
 
