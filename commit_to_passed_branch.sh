8a7d575648c29100d54ef69b57404e5702b19483
David Ohlemacher <ohlemacher@us.ibm.com>
2018-01-01 22:55:58 -0500

    git

--- commit_to_passed_branch.sh
+++ commit_to_passed_branch.sh
@@ -3,14 +3,9 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-if [ "$(hostname)" == "pluto.local" ]; then
-    echo "Configured for pluto execution"
-    declare -r cc_test_app_dir="cc_test_app"
-    declare -r cc_test_module_dir="cc_test_module"
-else
-    declare -r cc_test_app_dir="cc-test-app-repo"
-    declare -r cc_test_module_dir="cc-test-module-repo"
-fi
+declare -r gh_token=dbd9a1b24133b564320e4fd23e2b30504fbaa55a
+declare -r cc_test_app_dir="cc-test-app-repo"
+declare -r cc_test_module_dir="cc-test-module-repo"
 
 function info {
     local msg="$1"
@@ -31,6 +26,7 @@ function commit_to_branch {
     local -r br="$1"
     pushd "$cc_test_app_dir" > /dev/null
     local -r head="$(git rev-parse HEAD)"
+
     info "Commit to $br branch. Move HEAD to $head"
 
     info "+++ Use sed to fix fetches in .git/config"
@@ -39,44 +35,37 @@ function commit_to_branch {
     # ===== Info: +++ git checkout -b origin/passed_unit_tests
     # fatal: Cannot update paths and switch to branch 'passed_unit_tests' at the same time.
     # Did you intend to checkout 'origin/passed_unit_tests' which can not be resolved as commit?
-    sed -i -- 's?fetch = +refs/heads/develop:refs/remotes/origin/develop?fetch = +refs/heads/*:refs/remotes/origin/*?g' .git/config || die "git config fix failed"
+    #sed -i -- 's?fetch = +refs/heads/develop:refs/remotes/origin/develop?fetch = +refs/heads/*:refs/remotes/origin/*?g' .git/config || die "git config fix failed"
+    info "+++ Remove git origin" 
+    git remote rm origin               || die "git remote rm origin failed"
+
+    info "+++ Add git origin"
+    git remote add origin \
+        "https://github.com:$gh_token/cc_test_app.git" \
+                                       || die "Cannot add git origin" 
 
     info "+++ cat .git/config"
-    cat .git/config
+    cat .git/config                    || die "cat of git config failed"
 
     info "+++ git remote update"
     git remote update                  || die "git remote update failed"
-    git fetch
+    git fetch                          || die "git fetch failed"
     
-    info "+++ git branch -a"
-    git branch -a
-
-    info "+++ git branch -r"
-    git branch -r
-
-    info "+++ git remote -v"
-    git remote -v                      || die "git remote -v failed"
-
-    #info "+++ git fetch --all"
-    #git fetch --all                    || die "git fetch --all failed"
-
     info "+++ git fetch origin passed_unit_tests"
     git fetch origin passed_unit_tests || die "git fetch passed_unit_tests failed"
-    git pull  origin passed_unit_tests || die "git pull  passed_unit_tests failed"
-    #echo "+++ git fetch origin $br: $(git fetch origin "$br")"   || die "git fetch $br failed"
 
-    info "+++ git remote show origin"
-    git remote show origin             || die "git remote show origin failed"
-
-    info "git branch -avv"
-    git branch -avv                    || die "git branc -avv failed"
+    #info "+++ git remote show origin"
+    #git remote show origin             || die "git remote show origin failed"
 
     info "+++ git checkout -b origin/$br"
     git checkout -b "$br" "origin/$br" || die "git checkout $br failed"
+
     info "+++ git rebase $head"
-    git rebase "$head"                 || die "git rebase develop for $br failed"
+    git rebase "$head"                 || die "git rebase HEAD on $br branch failed"
+
     info "+++ git push $br"
     git push "origin $br"              || die "git push origin $br failed"
+
     popd > /dev/null
 }
 
